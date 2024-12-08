a32752260a60518d9c01ade9a2a969ae7c51b566
wqkereee <166634536+wqkereee@users.noreply.github.com>
2024-12-08 15:38:22 +0500

    Add files via upload

--- logic.py
+++ logic.py
@@ -1,23 +1,33 @@
 from random import randint
+
 import requests
+
 
 class Pokemon:
     pokemons = {}
     # Инициализация объекта (конструктор)
     def __init__(self, pokemon_trainer):
-
         self.pokemon_trainer = pokemon_trainer   
 
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
-
+        self.power = randint(1,100)
+        self.hp = randint(1,100)
+        
         Pokemon.pokemons[pokemon_trainer] = self
 
+        
     # Метод для получения картинки покемона через API
     def get_img(self):
-        pass
-    
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']['other']['official-artwork']['front_default'])
+        else:
+            return "https://static.wikia.nocookie.net/pokemon/images/0/0d/025Pikachu.png/revision/latest/scale-to-width-down/1000?cb=20181020165701&path-prefix=ru"
+
     # Метод для получения имени покемона через API
     def get_name(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
@@ -31,11 +41,33 @@ class Pokemon:
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f"Имя твоего покемона И его Статы: {self.name} {self.hp} {self.power}"
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
 
+    def attack(self, enemy  ):
+        if isinstance(enemy, wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+            change = randint(1,5)
+            if change == 1:
+                return "wizard use shield"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Битва @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            self.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}"
 
+class warior(Pokemon):
+    def attack(self,  enemy):
+        
+        superpower = randint(1,10)
+        self.power += superpower
+        result = super().attack(enemy)
+        self.power -= superpower
+        return result + f"\nБоец Применил супер атаку силой:{superpower} "
 
+class wizard(Pokemon):
+    pass
+        
--- main.py
+++ main.py
@@ -1,18 +1,49 @@
-import telebot 
+import telebot
+from random import randint
+
 from config import token
 
 from logic import Pokemon
+from logic import warior
+from logic import wizard
 
 bot = telebot.TeleBot(token) 
 
 @bot.message_handler(commands=['go'])
-def go(message):
+def start(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
-        pokemon = Pokemon(message.from_user.username)
+        chance = randint(1,3)
+        if chance == 1:
+            pokemon = Pokemon(message.from_user.username)
+        elif chance == 2:
+            pokemon = wizard(message.from_user.username)
+        elif chance == 3:
+            pokemon = warior(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
+
+
+@bot.message_handler(commands=['attack'])
+def attack_pok(message):
+    if message.reply_to_message:
+        if message.reply_to_message.from_user.username in Pokemon.pokemons.keys() and message.from_user.username in Pokemon.pokemons.keys():
+            enemy = Pokemon.pokemons[message.reply_to_message.from_user.username]
+            pok = Pokemon.pokemons[message.from_user.username]
+            res = pok.attack(enemy)
+            bot.send_message(message.chat.id, res)
+        else:
+            bot.send_message(message.chat.id, "Сражаться можно только с покемонами")
+    else:
+            bot.send_message(message.chat.id, "Чтобы атаковать, нужно ответить на сообщения того, кого хочешь атаковать")
+
+def start(message):
+    bot.send_message(message.chat.id, "Привет! Я бот для игры в покемонов, скорее попробуй создать себе покемона, нажимай - /go")
+
+
+
+
 
 
 bot.infinity_polling(none_stop=True)
